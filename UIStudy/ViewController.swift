b124c91faf2ca0d573f4e2ed0ba371db3be1ed0b
福成 毅 <takeshi.fukunari@recochoku.co.jp>
2017-05-15 17:50:43 +0900

    プロジェクトを作成

---
+++ UIStudy/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  UIStudy
+//
+//  Created by 福成毅 on 2017/05/15.
+//  Copyright © 2017年 福成毅. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
